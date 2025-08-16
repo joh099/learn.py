9267906bd5ac0de10ab7bb36412389b6e3558780
joh099 <juergenhubatsch@gmail.com>
2025-08-17 00:34:39 +0200

    Added temperature.py and modified start.py

--- start.py
+++ start.py
@@ -7,14 +7,14 @@ print( "Hello, my name is " + Name + " and I am " + age + " years old.")
 
 fuck_them = "lets get rich" 
 print(fuck_them)
-juergen_hubatsch = "futare billonair"
+juergen_hubatsch = "future billionaire"
 
 varbels = "values"  + "discription"
 my_city = "miami"
 stringa = " word"
 juergen_hubatsch_is_going_to_be_rich = True
 light_on = True 
-print('is ligt on ? ')
+print('is light on ? ')
 light_on = not True 
 print(  not light_on)
 
@@ -28,19 +28,19 @@ juergen_hubatsch1 = 44 == 44.8
 print("is juergen a bitch ")
 print(juergen_hubatsch1)
 greater_then = 77 < 88
-print("is 88 grater then 77 ?")
+print("is 88 greater than 77 ?")
 print(greater_then)
 gg = 66 
 ll = 99 
 Result_from_ll_and_gg = gg <= ll 
 print(Result_from_ll_and_gg)
-Types = "nummbe4rs strings "
+Types = "numbers strings "
 Numbers = "integers"
 float  = 8.999
 boolean = "true and False"
 assign = "storing a value in a variable"
 learn_type = 77
-print("waht is type ")
+print("what is type ")
 print(type(learn_type))
 fake_int = "88"
 convert = int(fake_int)
@@ -59,11 +59,11 @@ if juergen_hubatsch_is_going_to_be_rich is True:
 	
 print("__________________________________")
 print(Fore.WHITE + 'hey')
-nuber = 1
+number = 1
 numberr = 4 
 print("is the number bigger ?")
-numabers = nuber >= numberr 
-print( numabers)
+numbers = number >= numberr 
+print(numbers)
 jj = 'hh'
 kk = 'hh'
 print("is jj equal to kk ?")
@@ -75,9 +75,10 @@ result = '400 views'
 print("is the result equal to expected ?")
 print(result == Expected)
 print("__________________________________")
-print(Fore.GREEN + "Conditonals_Statments = 3") 
+print(Fore.GREEN + "Conditionals_Statements = 3") 
 print(Fore.WHITE + "________________________________")
-booleean = 'is to deside if or if not it shoud run a code block'
+booleean = 'is to decide if or if not it should run a code block'
+# if statement 
 if juergen_hubatsch_is_going_to_be_rich:
 	print("juergen is going to be rich")
 else:
@@ -92,8 +93,57 @@ else:
 	print("juergen does not have a job")
 
 
-if nuber > 55:
+if number > 55:
 	print("yes is bigger")
 else:
 	print("no is not bigger")
-jgjgjhgj
+
+
+Money = 100
+Money_needed = 2000
+if Money >= Money_needed:
+	print("juergen has enough money")
+else:
+	print("juergen does not have enough money")
+ 
+
+what_ia_a_elif_statement = 'is a statement that is a secondary condition that is checked if the first condition is false'
+
+
+print("__________________________________")
+
+if juergen_hubatsch_is_going_to_be_rich and light_on:
+	print("juergen is going to be rich and the light is on")
+
+scor = 10
+grade = "B"
+
+if scor == 100 or grade == "A":
+	print("juergen is a good student")
+elif scor >= 80:
+	print("juergen is a good student but not the best")	
+elif scor >= 50:
+	print("juergen is a student")
+else:
+	print("juergen is not a good student")	
+
+print("__________________________________")
+
+print(Fore.GREEN + " Loops = 4") 
+print(Fore.WHITE + "________________________________")
+
+money = 100
+print(money)
+money -= 10
+
+print(money)
+
+while money is not 0:
+	print("juergen has money")
+	money -= 10
+	print(money)
+	if money == 50:
+		print("juergen has 50 dollars left")
+		break
+
+
