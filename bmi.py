e57a0fa59b15705a2b64f4dec468cc46d7236569
joh099 <juergenhubatsch@gmail.com>
2025-08-18 20:09:46 +0200

    learing again

---
+++ bmi.py
@@ -0,0 +1,6 @@
+My_hight = 1.83  # in meters1.83 meters
+My_weight = 57  # in kilograms
+
+Bmi = My_weight / (My_hight ** 2)  # BMI formula
+print("My BMI is:", Bmi)  # Output the BMI value
+
--- temperature.py
+++ temperature.py
@@ -1,4 +1,6 @@
+# Fahrenheit to Celsius conversion
+# Formula: C = (F - 32) / 1.8
+
 fahrenheit = 79 
-celsius = fahrenheit - 32
-celsius = celsius /1.8 
+celsius = (fahrenheit - 32) /1.8
 print(celsius)
